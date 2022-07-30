32aa859fa493d9359b3fa1a64ebfa3a81f074ed3
BeakSungHyun <ces0135@naver.com>
2022-07-30 23:56:16 +0900

    표를 만드는 코드를 작성했는데, 동작하지 않으므로 수정이 필요하다.

--- profileDetail.js
+++ profileDetail.js
@@ -44,15 +44,47 @@ getIntroInput.addEventListener("keyup", function(event) {
     }
 })
 
-function addIntro() {
-    let introCnt = 1;
-    let newIntroSpan = document.createElement('span');
-    newIntroSpan.setAttribute("class", "introSpan");
-    newIntroSpan.setAttribute("id", "enterIntro" + introCnt);
-    introCnt += 1;
-
-    let newIntroText = document.getElementById("enterIntro" + introCnt);
-    let introTextToAdd = newIntroText.value;
-    newIntroSpan.innerText = introTextToAdd;
-    introTextToAdd.style.display = "none";
+function createSpan() {
+    let newSpan = document.createElement('span');
 }
+
+function createInput() {
+    document.createElement('input');
+}
+
+
+
+
+
+function addIntro() {
+    let introCnt = 2;
+
+    let newIntroSpan = document.createElement('span');  //input 감싸는 span
+    newIntroSpan.setAttribute('id', 'introSpan_' + introCnt);
+    
+
+    let introInput = document.createElement('input');  //입력받는 input 창
+    introInput.setAttribute('id', 'introInput_' + introCnt);
+    introInput.setAttribute('class', 'setIntro');
+    
+    newIntroSpan.appendChild(introInput);  //<span><input id="introInput_introCnt"></span>
+    let introTxt = document.getElementById('introInput_' + introCnt).value;  //input 창에 입력한 텍스트
+
+    newIntroSpan.innerText = introTxt;  //입력한 테스트 span 안에 추가
+    document.getElementById('introInput_' + introCnt).style.display = "none";  //입력 후 input 창 없애기
+
+    let introContainer = document.getElementsByClassName('personalIntro');  //최상위 div
+    introContainer.appendChild(newIntroSpan);
+    introCnt++;
+}
+
+
+
+//input 창에서 enter를 누르면 addIntro() 호출
+let introInputForEvent = document.getElementsByClassName('setIntro');
+introInputForEvent.addEventListener('keyup', function(event) {
+    if(event.keyCode === 13) {
+        alert('enter');  //check용 -> 왜 안 되냐
+        addIntro();
+    }
+})
